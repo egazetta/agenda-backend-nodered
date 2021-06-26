9ac2776208f520f4a4e2d9fecdadb6135cc3adb5
Edson Gazetta <egazetta@gmail.com>
2021-06-26 18:15:40 -0300

    env file

--- settings.js
+++ settings.js
@@ -11,6 +11,8 @@
  *    https://nodered.org/docs/user-guide/runtime/configuration
  **/
 
+require('dotenv').config()
+
 module.exports = {
     // the tcp port that the Node-RED web server is listening on
     uiPort: process.env.PORT || 1880,
